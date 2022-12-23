47e4f50768c2c265bff434d98283a5860699b4cb
Daniel Mundra <daniel.mundra@civicactions.com>
2022-12-23 08:42:33 -0800

    Issue #19: Added github action to run cypress tests on pull requests. Modified config to generate junit reports.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/junit.[hash].xml',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
