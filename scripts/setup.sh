929e775f3bc9b3d806301d24d5d05c013dacfc4d
Daniel Mundra <danielmundra@gmail.com>
2023-04-09 18:41:52 -0700

    Moved copy config config after ddev config setup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,6 @@
 # Create directory for Drupal 10 so we don't blow away all the files in the
 # main directory.
 mkdir drupal
-# Copy configuration into the Drupal directory. Any configuration changes will require a copying back to the main directory.
-cp -r config drupal/.
 cd drupal || exit
 
 # Taken from https://ddev.readthedocs.io/en/stable/users/quickstart/
@@ -12,7 +10,11 @@ ddev config --project-type=drupal10 --docroot=web --create-docroot
 ddev start
 ddev composer create drupal/recommended-project
 ddev composer require drush/drush
+
+# Copy configuration into the Drupal directory. Any configuration changes will require a copying back to the main directory.
+cp -r ../config .
 ddev exec echo '\$settings[\"config_sync_directory\"] = \"../config/sync\"\;' >> web/sites/default/settings.php
+
 ddev drush site:install minimal --account-name=admin --account-pass=admin -y --existing-config
 
 # Return to the parent directory.
