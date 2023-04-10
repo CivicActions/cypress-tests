a91b27d4d8a9cc26e8569c7cb05450be76a5c7aa
Daniel Mundra <danielmundra@gmail.com>
2023-04-09 19:41:02 -0700

    Changed bold selector again.

--- cypress/e2e/3-civicactions-examples/drupal-ckeditor.cy.js
+++ cypress/e2e/3-civicactions-examples/drupal-ckeditor.cy.js
@@ -57,7 +57,7 @@ describe('CKEditor tests', () => {
     cy.get('.ck-content').type('{selectall}')
 
     // Click bold button.
-    cy.get('.ck-button').contains('Bold').click()
+    cy.get('[data-cke-tooltip-text*="Bold"]').click()
 
     // Save page.
     cy.get('#edit-submit').click()
