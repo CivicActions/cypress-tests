925586b6c3d992c393dad90655bf2f3299b6e77d
Daniel Mundra <daniel.mundra@civicactions.com>
2023-04-21 14:53:41 -0700

    Added cypress-file-upload plugin and CKEditor image upload tests with test image.

--- cypress/e2e/3-civicactions-examples/drupal-ckeditor.cy.js
+++ cypress/e2e/3-civicactions-examples/drupal-ckeditor.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import 'cypress-real-events/support'
+import 'cypress-file-upload'
 
 describe('CKEditor tests', () => {
   beforeEach(() => {
@@ -130,6 +131,104 @@ describe('CKEditor tests', () => {
     cy.get('.field--name-body iframe')
       .its('0.contentDocument.body')
       .should('not.be.empty')
+
+    // Log out from being an administrator.
+    cy.logout()
+  })
+
+  it('Logs in and create a page with an image inserted with the insert image button', () => {
+    const title = 'CKEditor Test 4'
+
+    // Log in as an administrator.
+    cy.login('admin', 'admin')
+
+    // Go to create page form.
+    cy.visit('/node/add/page')
+
+    // Enter test in title.
+    cy.get('input[name="title[0][value]"]').type(title)
+
+    // Intercept CKEditor5 upload image.
+    cy.intercept('/ckeditor5/upload-image/basic_html*').as('imageUpload')
+
+    // Upload file to hidden file input
+    cy.get('input[type="file"]').attachFile('test_image.jpg')
+
+    // Wait for image upload.
+    cy.wait('@imageUpload')
+
+    // Save page.
+    cy.get('#edit-submit').click()
+
+    // View page and confirm the title text.
+    cy.get('h1.page-title').contains(title)
+
+    // View page and confirm the image exists and has the appropriate width.
+    cy.get('.field--name-body img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.equal(150)
+      })
+
+    // Log out from being an administrator.
+    cy.logout()
+  })
+
+  it('Logs in and create a page with an image inserted with the insert media button', () => {
+    const title = 'CKEditor Test 5'
+
+    // Log in as an administrator.
+    cy.login('admin', 'admin')
+
+    // Go to create page form.
+    cy.visit('/node/add/page')
+
+    // Enter test in title.
+    cy.get('input[name="title[0][value]"]').type(title)
+
+    // Select full HTML body format.
+    cy.get('select[name="body[0][format]"]').select('Full HTML')
+
+    // Click show more items button.
+    cy.get('[data-cke-tooltip-text="Show more items"]').click()
+
+    // Click insert media button.
+    cy.get('[data-cke-tooltip-text="Insert Media"]').click()
+
+    // Upload file to the add file input
+    cy.get('input[name="files[upload]"]').attachFile('test_image.jpg')
+
+    // Type alternate text
+    cy.get('input[name="media[0][fields][field_media_image][0][alt]"]').type(
+      'Fen selfie at a rally.'
+    )
+
+    // Click save button.
+    cy.get('button').contains('Save').click()
+
+    // Click insert selected button.
+    cy.get('button').contains('Insert selected').click()
+
+    // Intercept Drupal media.
+    cy.intercept('/media/full_html/preview*').as('drupalMedia')
+
+    // Wait for Drupal media.
+    cy.wait('@drupalMedia')
+
+    // Save page.
+    cy.get('#edit-submit').click()
+
+    // View page and confirm the title text.
+    cy.get('h1.page-title').contains(title)
+
+    // View page and confirm the image exists and has the appropriate width.
+    cy.get('.field--name-body img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.equal(150)
+      })
 
     // Log out from being an administrator.
     cy.logout()
