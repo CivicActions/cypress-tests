df1f442ec55b78cce852b951011efc2d9ceb093f
Daniel Mundra <daniel.mundra@civicactions.com>
2026-01-14 07:37:37 -0800

    Switched lighthouse library to https://github.com/TrevorBurnham/cypress-lighthouse-plugin.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import '@cypress-audit/lighthouse/commands'
+import 'cypress-lighthouse-plugin/commands'
 
 Cypress.Commands.add('login', (user, password) => {
   return cy.session(
